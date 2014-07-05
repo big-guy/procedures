789ea3d083d5abfb9414165f3e175f8bc421db1d
Sterling Greene <sterling.greene@gmail.com>
2014-07-05 13:36:14 -0400

    split out html template

--- build.sh
+++ build.sh
@@ -2,12 +2,40 @@
 
 declare project=$1
 declare dataFile=${project}/data.json
-declare mainFile=${project}/template.mustache
+declare templateFile=${project}/template.mustache
 declare outdir=${project}/build
-declare indexOut=${outdir}/index.md
+declare mainOut=${outdir}/main.md
+declare bodyOut=${outdir}/body.html
 
 mkdir -p $outdir
 
-pystache $mainFile $dataFile > ${indexOut}
+# HTML Template
+# -------------
 
-markdown_py -x extra -x toc -x sane_lists -x meta ${indexOut} --output_format=html5 --file=${outdir}/index.html
+# Create a function for apply the standard [Docco][do] HTML layout, using
+# [jashkenas][ja]'s gorgeous CSS for styles. Wrapping the layout in a function
+# lets us apply it elsewhere simply by piping in a body.
+#
+# [ja]: http://github.com/jashkenas/
+# [do]: http://jashkenas.github.com/docco/
+layout () {
+    cat <<HTML
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv='content-type' content='text/html;charset=utf-8'>
+    <title>$1</title>
+    <link rel=stylesheet href="http://jashkenas.github.com/docco/resources/docco.css">
+</head>
+<body>
+$(cat)
+</body>
+</html>
+HTML
+}
+
+pystache $templateFile $dataFile > ${mainOut}
+
+markdown_py -x extra -x toc -x sane_lists -x meta ${mainOut} | layout "test" 
+
+
